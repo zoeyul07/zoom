4144048ae47a65973f33c80121b8f5a6faa8f9bf
Seoyul Kim <zoeyul07@gmail.com>
2024-07-30 15:50:16 +0900

    feat: 실시간 채팅 어플 생성을 위한 express, pug 설정

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Noom
 
 Zoom Clone using Nodejs, WebRTC and Websockets
+
+- mvp.css
+- pug
+- express
+- 프로젝트 변경사항이 있을시 서버를 재시작하도록 Nodemon 사용
+  - 프로젝트 내에서는 babel-node를 실행하도록 한다.
+- 작성한 코드를 일반 Nodejs 코드로 컴파일 해주도록 babel 사용
--- src/server.js
+++ src/server.js
@@ -2,4 +2,18 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000);
+//setView
+//pug 로 view engine 설정
+app.set("view engine", "pug");
+//express 에 template 이 어디있을건지 지정
+app.set("views", __dirname + "/views");
+//public url을 생성해서 유저에게 파일 공유
+app.use("/public", express.static(__dirname + "/public"));
+
+//render view
+//home.pug 를 render 해주는 Route handler 생성
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+//listen
+app.listen(3000, handleListen);
